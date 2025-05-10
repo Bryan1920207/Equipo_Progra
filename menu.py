e7aed29943d485595a1fa478d886443accf8471f
Bryan Eduardo Niño Contreras <bryan.ninocn@uanl.edu.mx>
2025-05-09 21:35:38 -0600

    Falta opcion 2(comprobar)

--- menu.py
+++ menu.py
@@ -17,21 +17,61 @@ def Buscar(idProducto):
         return index_buscar
     
 lista_productos=[]
-    
+
 print("1.- Agregar Producto\n" "2.- Agregar Comentario" "3.- Ver Producto\n" "4.- Ver Productos\n" "5.- Eliminar Producto\n" "6.- Modificar Producto\n" "7.- Salir")
 opcion=int(input("Que opcion elijes? "))
 while True:
     if opcion ==1:
-        pass
+        idProducto=input("idProducto? ")
+        Descripcion=input("Descripcion? ")
+        Marca=input("Marca? ")
+        Comentarios=input("Comentarios? ")
+        productoNuevo=Productos(idProducto,Descripcion,Marca,Comentarios)
+        lista_productos.append(productoNuevo)
     elif opcion==2:
-        pass
+        idProducto=input("Id del producto a añadir comentarios: ")
+        i=buscar(idProducto)
+        datosProducto=lista_productos[i]
+        Comentario=input("Id del producto a añadir comentarios: ")
+        Productos.append(Comentario)
+        datosProducto.imprimir()
     elif opcion==3:
-        pass
+        idProducto=input("Id del producto a buscar: ")
+        i=buscar(idProducto)
+        datosProducto=lista_productos[i]
+        datosProducto.imprimir()
     elif opcion==4:
-        pass
+        for producto in lista_productos:
+            producto.imprimir()
     elif opcion==5:
-        pass
+        idProducto=input("Id del producto a borrar: ")
+        i=buscar(idProducto)
+        if i=-1:
+            print("No existe el producto")
+        else:
+            print("Producto a borrar")
+            datosProducto=lista_productos[i]
+            datosProducto.imprimir()
+            confirmar=input("Estas seguro de querer borrarlo? ")
+            if confirmar=="S":
+                lista_productos.pop(i)
     elif opcion==6:
-        pass
+        idProducto=input("Id del producto a modificar: ")
+        i=buscar(idProducto)
+        if i=-1:
+            print("No existe el producto")
+        else:
+            print("Datos actuales")
+            datosProducto=lista_productos[i]
+            datosProducto.imprimir()
+            confirmar=input("Estas seguro de querer modificarlo? ")
+            if confirmar=="S":
+                print("Introduce los nuevos datos")
+                idProducto=input("idProducto? ")
+                Descripcion=input("Descripcion? ")
+                Marca=input("Marca? ")
+                Comentarios=input("Comentarios? ")
+                productoNuevo=Productos(idProducto,Descripcion,Marca,Comentarios)
+                lista_productos.append(productoNuevo[i])
     elif opcion==7:
         break
